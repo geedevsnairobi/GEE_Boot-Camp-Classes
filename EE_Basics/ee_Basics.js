c83605c7e6287ccf313fea736c549d912e7f1ff9
geedevsnairobi <119797704+geedevsnairobi@users.noreply.github.com>
2023-10-14 14:29:20 +0300

    eebasics

---
+++ EE_Basics/ee_Basics.js
@@ -0,0 +1,33 @@
+
+// // First Print 
+print ('Hi GEE Devs Community Nairobi, This is our first EE Class!');
+
+// Define variables and assigning string values
+var Community = 'GEE Nairobi';
+print(Community);
+
+// Assigning numeric values
+var EventsDone = 102;
+print(EventsDone);
+
+// Define Lists are writen using the Squre brackets and can hold multiple values
+var GEEDevsEventTypes = ['Expert_Talks', 'Hands-on', 'Bootcamp-Class'];
+print (GEEDevsEventTypes);
+
+// Working with Dictionaries 
+/* Objects in JavaScript allow you to store key-value pairs, where each value can be referred to by its key. You can create a dictionary using the curly braces {}. The code below creates an object called cityData with some information about San Francisco.*/ 
+// Dictionary
+
+var geeDevs = {
+  'Community': 'Community',
+  'Subscribers': 1200,
+  'Events': 102,
+};
+print(geeDevs);
+
+
+// //---Types of JavaScript comments---
+
+// /* Single Line Comment: It is represented by // This is a single line comment
+// Multi-Line Comment: We use Slash and asterisk symbol /* This paragrap is a multline commend */
+ 
